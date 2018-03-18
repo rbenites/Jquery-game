0b4b756066103c3140b5baf916b990f682913ae8
rbenites <rbenites115@gmail.com>
2018-03-17 17:19:25 -0700

    fixed the onload, random number generators and got the comp num to print

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,32 +2,22 @@
 
 //Global variables
 var wins=0, loses=0;
-var compRandNum=0;
 var playerTotalScore=0;
 var ruby=0;sapphire=0;sapphireYellow=0;emerald=0;
 var gemTotal=0;
-
-
-$(document).ready(() => {
-    $('#ruby').on('click',()=>{
-
-    })
-
-
-//random number generator that the player will guess
-function random_num(){
-    Math.floor((Math.random()*120)+19);
-}
-
 //random number generator for the crystals
-function random_crystal_num(){
-Math.floor((Math.random()*12)+1);
-}
-//add an event listener to watch for the button click on the gems
-$('col-xs-2 col-md-3').on("click",()=>{
+var crystalRandNum= Math.floor(Math.random() * 13)+1;
+console.log(crystalRandNum);
+//random number generator that the computer generates
+var compRandNum= Math.floor(Math.random()*121)+1;
+console.log(compRandNum);
 
-)}
+   
 
+window.onload = function() {
 
+//this will generate a random number and then display it at #random_number
+$('#random_number').html("<h1>" + compRandNum+ "</h1>");
+   
 
-}
+};
